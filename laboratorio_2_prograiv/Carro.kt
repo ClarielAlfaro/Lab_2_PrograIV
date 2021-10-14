b334b5d851dad891d9c8dbeb73a3a2c3a06ae8ff
ClarielAlfaro <av0255032019@unab.edu.sv>
2021-10-14 11:49:53 -0600

    Herencia

---
+++ laboratorio_2_prograiv/Carro.kt
@@ -0,0 +1,7 @@
+package com.example.laboratorio_2_prograiv
+
+
+private open class Carro : Vehiculo() {
+
+
+}
---
+++ laboratorio_2_prograiv/Vehiculo.kt
@@ -0,0 +1,12 @@
+package com.example.laboratorio_2_prograiv
+
+open class Vehiculo {
+
+    var marca: String = ""
+    var color: String = ""
+    var pasajeros: Int = 0
+    var placa: String = ""
+
+
+
+}
